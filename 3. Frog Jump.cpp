9f74aae1b88bc1c133ed8696bd79a065bdc4890d
Abrar Rafid Noor <142677434+abrarrn@users.noreply.github.com>
2023-09-24 22:53:58 +0600

    Frog jump from Coding Ninjas

---
+++ 3. Frog Jump.cpp
@@ -0,0 +1,31 @@
+#include <bits/stdc++.h>
+int dp[1001];
+
+int recurr(int n, vector<int> &heights){
+    if(n == 0)      return 0;
+    if(dp[n] != -1) return dp[n];
+    int step1 = recurr(n-1, heights) + abs(heights[n]-heights[n-1]);
+    int step2 = INT_MAX;
+    if(n > 1)
+        step2 = recurr(n-2, heights) + abs(heights[n]-heights[n-2]);
+    return dp[n] = min(step1, step2);
+}
+
+int frogJump(int n, vector<int> &heights)
+{
+    //memoization
+    vector<int> dp(n, 0);
+    return recurr(n-1, heights);
+
+    //space-optimized
+    vector<int> dp(n, 0);
+    dp[0] = 0;
+    for(int i=1; i<=n-1; ++i){
+        int step1 = dp[i-1] + abs(heights[i]-heights[i-1]);
+        int step2 = INT_MAX;
+        if(i > 1)
+            step2 = dp[i-2] + abs(heights[i]-heights[i-2]);
+        dp[i] = min(step1, step2);
+    }
+    return dp[n-1];
+}
